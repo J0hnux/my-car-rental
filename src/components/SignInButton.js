cdff6c0267d94a3e3b7d6a8d62224fb9b9b2f07f
J0hnux <jc24.doliente@gmail.com>
2024-06-02 00:35:58 +0800

    Tested Login capabilities with Google

--- src/components/SignInButton.js
+++ src/components/SignInButton.js
@@ -1,22 +1,29 @@
-"use client"
+"use client";
 import { signIn, signOut, useSession } from "next-auth/react";
 
-
 const SignInButton = () => {
-    const { data: session } = useSession();
+  const { data: session } = useSession();
 
-    if(session && session.user) {
-        return (
-            <div className="flex gap-4 ml-auto">
-                <p className="text-sky-600">{session.user.name}</p>
-                <button onClick={() => signOut()} className="text-red-600">Sign Out</button>
-            </div>
-        )
-    }
-    
+  if (session && session.user) {
     return (
-        <button className="text-green-600 ml-auto" onClick={() => signIn()}>Sign In</button>
-    )
-}
+      <div className="flex gap-1 ml-auto">
+        {/* ================ TODO ============== */}
+        {/* When p is hovered, it changes to a Sign-out button */}
+        <p className="text-sky-600">
+          Welcome <span className="block">{session.user.name}</span>
+        </p>
+        <button onClick={() => signOut()} className="text-red-600">
+          Sign Out
+        </button>
+      </div>
+    );
+  }
 
-export default SignInButton
+  return (
+    <button className="text-green-600 ml-auto" onClick={() => signIn("google")}>
+      Sign In
+    </button>
+  );
+};
+
+export default SignInButton;
