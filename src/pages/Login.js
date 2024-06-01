548ffa32d1ab43951ea8866a57b8f84a4df55523
J0hnux <jc24.doliente@gmail.com>
2024-06-02 00:36:29 +0800

    Added Login page

---
+++ src/pages/Login.js
@@ -0,0 +1,27 @@
+import "../styles/Login.scss";
+
+export default function Login() {
+  return (
+    <div className="login-container">
+      <p className="login_header">Login</p>
+      <label htmlFor="email">Your email address</label>
+      <input type="email" />
+      <input type="password" placeholder="Password" />
+      <div className="forgot_pass">
+        <a href="#">Forgot your password?</a>
+      </div>
+      <div className="check_button">
+        <span>
+          <input type="checkbox" /> Remember me
+        </span>
+        <button>Log in</button>
+      </div>
+      <div className="signup-area">
+        <p>Don{"'"}t have an account?&nbsp;</p>
+        <span>
+          <a href="#">Create one now</a>
+        </span>
+      </div>
+    </div>
+  );
+}
