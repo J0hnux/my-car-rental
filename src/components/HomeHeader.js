876de76c6ec58707f4ff088c4e38af2eb0c06bd7
J0hnux <jc24.doliente@gmail.com>
2024-06-02 00:35:15 +0800

    Added homeheader which will serve as the top panel with nav bars

---
+++ src/components/HomeHeader.js
@@ -0,0 +1,45 @@
+import Link from "next/link";
+import "./HomeHeader.scss";
+import SignInButton from "./SignInButton";
+
+const HomeHeader = () => {
+  return (
+    <div className="header-container">
+      <button className="mobile_nav">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        >
+          <line x1="3" y1="12" x2="21" y2="12"></line>
+          <line x1="3" y1="6" x2="21" y2="6"></line>
+          <line x1="3" y1="18" x2="21" y2="18"></line>
+        </svg>
+      </button>
+      <h1>Clark Rentals</h1>
+      <nav>
+        <ul>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+        </ul>
+      </nav>
+      <Link href="/Login">Login</Link>
+      {/* <SignInButton /> */}
+    </div>
+  );
+};
+
+export default HomeHeader;
