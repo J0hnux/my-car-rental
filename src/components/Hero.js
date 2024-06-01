65b968f88424c92cc263dd93044ce9e7c01dcb0a
J0hnux <jc24.doliente@gmail.com>
2024-06-02 00:34:18 +0800

    Added Hero page and provided a few contents

---
+++ src/components/Hero.js
@@ -0,0 +1,21 @@
+import "./Hero.scss";
+
+const Hero = () => {
+  return (
+    <div className="hero container section">
+      <div className="hero_left_panel">
+        <h1>Hero</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur,
+          blanditiis id officia illum iure nulla incidunt ea similique hic saepe
+          necessitatibus dicta dolorum quam sunt animi quia sit vitae quaerat?
+        </p>
+      </div>
+      <div className="hero_img_container">
+        <img src="/Hero.png" alt="hero.png" />
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
