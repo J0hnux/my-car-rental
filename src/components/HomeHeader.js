2b348c825b7b39d5e0bed3e6c58c879d6a415cb9
J0hnux <jc24.doliente@gmail.com>
2024-06-08 21:03:45 +0800

    Added Socials Panel

--- src/components/HomeHeader.js
+++ src/components/HomeHeader.js
@@ -1,43 +1,51 @@
 import Link from "next/link";
 import "./HomeHeader.scss";
 import SignInButton from "./SignInButton";
+import SocialsPanel from "./SocialsPanel";
 
 const HomeHeader = () => {
   return (
     <div className="header-container">
-      <button className="mobile_nav">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        >
-          <line x1="3" y1="12" x2="21" y2="12"></line>
-          <line x1="3" y1="6" x2="21" y2="6"></line>
-          <line x1="3" y1="18" x2="21" y2="18"></line>
-        </svg>
-      </button>
-      <h1>Clark Rentals</h1>
-      <nav>
-        <ul>
-          <li>
-            <a href="#">Home</a>
-          </li>
-          <li>
-            <a href="#">About</a>
-          </li>
-          <li>
-            <a href="#">Contact</a>
-          </li>
-        </ul>
-      </nav>
-      <Link href="/Login">Login</Link>
-      {/* <SignInButton /> */}
+      <SocialsPanel />
+      <div className="main-header">
+        <button className="mobile_nav">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <line x1="3" y1="12" x2="21" y2="12"></line>
+            <line x1="3" y1="6" x2="21" y2="6"></line>
+            <line x1="3" y1="18" x2="21" y2="18"></line>
+          </svg>
+        </button>
+        <h1>
+          <Link className="logo-text" href="/">
+            CLARK CARS
+          </Link>
+        </h1>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link href="/About">About</Link>
+              </li>
+              <li>
+                <Link href="/Contact">Contact</Link>
+              </li>
+              <li>
+                <Link href="/Login">Inquire Now</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
     </div>
   );
 };
