970d65fc66bf144e556ac2804a895f24d7534f55
J0hnux <jc24.doliente@gmail.com>
2024-06-08 21:05:08 +0800

    Changed everything and replaced with an image covering entire space

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,19 +2,9 @@ import "./Hero.scss";
 
 const Hero = () => {
   return (
-    <section className="hero container">
-      <div className="hero_left_panel">
-        <h1>Hero</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur,
-          blanditiis id officia illum iure nulla incidunt ea similique hic saepe
-          necessitatibus dicta dolorum quam sunt animi quia sit vitae quaerat?
-        </p>
-      </div>
-      <div className="hero_img_container">
-        <img src="/Hero.png" alt="hero.png" />
-      </div>
-    </section>
+    <div className="hero">
+      <img src="/Hero.png" alt="hero.png" />
+    </div>
   );
 };
 
