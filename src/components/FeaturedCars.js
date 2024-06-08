c49995e503061fea6c82a9d4d32936788e301fd6
J0hnux <jc24.doliente@gmail.com>
2024-06-08 21:05:53 +0800

    Added image carousel, to be fixed

--- src/components/FeaturedCars.js
+++ src/components/FeaturedCars.js
@@ -4,32 +4,32 @@ const FeaturedCars = () => {
   return (
     <section className="featured container">
       <h2>Featured Cars</h2>
-      <div className="image_carousel">
-        <div class="image-slide fade">
+      <div className="image_carousel" aria-label="Featured Cars Image Carousel">
+        <div class="image-slide fade ">
           <img
             src="Featured_Cars/mercedes_benz_white.png"
             alt="mercedes_benz"
           />
-          <div class="text">Mercedes Benz</div>
+          <div class="image-text">Mercedes Benz</div>
         </div>
 
-        <div class="image-slide fade">
+        <div class="image-slide fade hidden">
           <img src="Featured_Cars/ford_mustang_blue.png" alt="ford_mustang" />
-          <div class="text">Ford Mustang</div>
+          <div class="image-text">Ford Mustang</div>
         </div>
 
-        <div class="image-slide fade">
+        <div class="image-slide fade hidden">
           <img
             src="Featured_Cars/toyota_fortuner_white.png"
             alt="toyota_fortuner"
           />
-          <div class="text">Toyota Fortuner</div>
+          <div class="image-text">Toyota Fortuner</div>
         </div>
 
-        <a class="prev" onclick="plusSlides(-1)">
+        <a class="prev" aria-label="previous image" onclick="{plusSlides(-1)}">
           &#10094;
         </a>
-        <a class="next" onclick="plusSlides(1)">
+        <a class="next" aria-label="next image" onclick="{plusSlides(1)}">
           &#10095;
         </a>
       </div>
