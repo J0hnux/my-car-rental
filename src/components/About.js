1496027fb85d9ddc46b9d7f7bd8f1d0f82ef3b96
J0hnux <jc24.doliente@gmail.com>
2024-06-10 15:46:23 +0800

    About section implementation

---
+++ src/components/About.js
@@ -0,0 +1,57 @@
+import "./About.scss";
+
+const About = () => {
+  return (
+    <section className="about-sect">
+      <h1>Clark Cars Philippines</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis dolores
+        architecto iste, accusantium sit eius recusandae corporis id fugiat, nam
+        voluptas provident voluptatum vero reiciendis dignissimos atque sequi
+        delectus ipsa. Vitae velit aliquam eaque quis magnam, accusamus
+        obcaecati dolorem tempore consequatur quisquam sit blanditiis
+        perferendis possimus ut.
+      </p>
+      <p>
+        Illo maiores impedit officiis facere quibusdam aliquam ea nisi id
+        maxime, tempora hic assumenda ducimus, obcaecati sapiente fuga modi
+        provident consectetur cumque? Sequi tenetur veniam sed necessitatibus
+        laborum fuga explicabo asperiores ea amet exercitationem alias voluptas,
+        quo dicta totam quod! Corporis molestias nulla saepe voluptatibus animi
+        tempora tempore reiciendis quasi incidunt.
+      </p>
+      <p>
+        Corporis rerum nostrum quam beatae sint, porro earum, quod tempora
+        facere perferendis optio maiores soluta ullam impedit, repellendus alias
+        esse aut suscipit sit. Beatae, placeat nemo? Quidem, aut reprehenderit
+        blanditiis ut, recusandae harum nihil unde nisi corrupti dolore
+        asperiores explicabo a. Atque natus harum officia sunt eius magnam
+        voluptatum, ex eaque mollitia beatae nam voluptas consectetur delectus
+        possimus explicabo ipsa quia doloribus aperiam, suscipit minima
+        repellendus quam eligendi error. Eius totam corporis perferendis saepe
+        tempore quis non officia voluptatum officiis temporibus! Nesciunt
+        expedita laborum reiciendis repellendus quidem earum necessitatibus
+        libero iste accusantium eveniet ut sit a beatae, unde, voluptas officia
+        quod corporis ea!
+      </p>
+      <p>
+        Ipsam mollitia totam assumenda quaerat. Minus animi eum, totam quas
+        impedit quo libero odio nemo eveniet inventore, quasi nihil modi
+        consectetur quod aliquid molestias voluptates unde, culpa nobis veniam
+        sapiente! Molestias laborum amet totam consectetur odio voluptate quas
+        voluptas officiis obcaecati, quae quibusdam illum commodi distinctio
+        consequuntur maiores sapiente nihil vel.
+      </p>
+      <p>
+        Beatae aut exercitationem nesciunt tempore nam perferendis eum,
+        voluptatibus tenetur! Eius hic eum similique vitae saepe praesentium,
+        eos officia et magnam dolore tempora magni suscipit ratione corrupti
+        doloribus numquam recusandae adipisci eaque eligendi esse ab commodi
+        maiores. Eaque fugiat alias corporis nesciunt? Natus quam optio
+        reiciendis similique commodi!
+      </p>
+    </section>
+  );
+};
+
+export default About;
