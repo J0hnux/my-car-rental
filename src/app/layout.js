b21ceb98c89f07dd276eec7189271b4829ea7dc2
J0hnux <jc24.doliente@gmail.com>
2024-06-12 22:48:53 +0800

    Added Footer and Testimonials

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Car Rental",
-  description: "Car Rental",
+  description: "Clark Cars Rental Philippines",
 };
 
 export default function RootLayout({ children }) {
