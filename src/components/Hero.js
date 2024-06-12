148ddb9236106e474b3fba3dfc11011fa0d1a9a0
J0hnux <jc24.doliente@gmail.com>
2024-06-12 22:21:40 +0800

    Planning to change hero to image slider rather than a boring single image

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,7 +3,13 @@ import "./Hero.scss";
 const Hero = () => {
   return (
     <div className="hero">
-      <img src="/Hero.png" alt="hero.png" />
+      <div className="hero-image-slider">
+        <img src="/Hero.jpg" alt="luxury car image" />
+      </div>
+      <div className="hero-content">
+        <h1>Looking to drive your own dream car?</h1>
+        <button>Inquire Now</button>
+      </div>
     </div>
   );
 };
