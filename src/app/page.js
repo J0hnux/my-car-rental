36501d2b28105b7af57345c385f78ba34a2f0624
J0hnux <jc24.doliente@gmail.com>
2024-06-12 22:49:04 +0800

    Added Footer and Testimonials

--- src/app/page.js
+++ src/app/page.js
@@ -3,13 +3,15 @@ import About from "@/components/About";
 import Brands from "@/components/Brands";
 import Categories from "@/components/Categories";
 import FeaturedCars from "@/components/FeaturedCars";
+import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
 import HomeHeader from "@/components/HomeHeader";
 import Testimonials from "@/components/Testimonials";
+import { GlobalProvider } from "@/context/GlobalState";
 
 export default function Home() {
   return (
-    <div className="">
+    <GlobalProvider>
       <div className="text-center w-full">
         <HomeHeader />
         <Hero />
@@ -18,7 +20,8 @@ export default function Home() {
         <Categories />
         <Brands />
         <Testimonials />
+        <Footer />
       </div>
-    </div>
+    </GlobalProvider>
   );
 }
