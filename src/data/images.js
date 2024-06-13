3a83c4f942b957788069b37cbc8420162122a934
J0hnux <jc24.doliente@gmail.com>
2024-06-13 20:22:56 +0800

    Added total of 4 images for hero image slider section

---
+++ src/data/images.js
@@ -0,0 +1,20 @@
+const images = [
+  {
+    imgURL: "/Hero.jpg",
+    imgAlt: "hero 1",
+  },
+  {
+    imgURL: "/Hero2.png",
+    imgAlt: "hero 2",
+  },
+  {
+    imgURL: "/Hero3.png",
+    imgAlt: "hero 3",
+  },
+  {
+    imgURL: "/Hero4.png",
+    imgAlt: "hero 4",
+  },
+];
+
+export default images;
