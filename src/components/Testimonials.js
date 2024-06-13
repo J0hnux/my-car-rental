7902dfab73ac56100f62f5cdb75ed79a2d557060
J0hnux <jc24.doliente@gmail.com>
2024-06-13 20:25:58 +0800

    Basic testimonials implementation

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -5,19 +5,21 @@ import "./Testimonials.scss";
 const Testimonials = () => {
   return (
     <section className="testimonials">
-      <h1>Testimonials</h1>
-      <div className="testimonials-content">
-        {testimonials.map((testimony, index) => {
-          return (
-            <TestimonialCard
-              key={index}
-              imgUrl={testimony.imgURL}
-              imgAlt={testimony.imgAlt}
-              name={testimony.name}
-              testimony={testimony.testimony}
-            />
-          );
-        })}
+      <div className="testimonials-container">
+        <h1>Testimonials</h1>
+        <div className="testimonials-content">
+          {testimonials.map((testimony, index) => {
+            return (
+              <TestimonialCard
+                key={index}
+                imgUrl={testimony.imgURL}
+                imgAlt={testimony.imgAlt}
+                name={testimony.name}
+                testimony={testimony.testimony}
+              />
+            );
+          })}
+        </div>
       </div>
     </section>
   );
