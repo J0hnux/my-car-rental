fde808080ed8e65c80bfef27a9cb76d2ef4cd348
J0hnux <jc24.doliente@gmail.com>
2024-06-13 14:40:21 +0800

    The data containing testimonials information for mapping use

---
+++ src/data/testimonials.js
@@ -0,0 +1,25 @@
+const testimonials = [
+  {
+    imgURL: "/Testimonials/my-pic2.jpeg",
+    imgAlt: "clark's testimony",
+    name: "Clark Doliente",
+    testimony:
+      "Rented a Mercedes Benz 3 months ago and it came right on time. I will definitely rent again if given the chance.",
+  },
+  {
+    imgURL: "/Testimonials/amara.png",
+    imgAlt: "amara's testimony",
+    name: "Amara Redfield",
+    testimony:
+      "The cars they put on rent here are top of the line and well maintained. I highly recommend this car rental shop.",
+  },
+  {
+    imgURL: "/Testimonials/daniel.png",
+    imgAlt: "daniel's testimony",
+    name: "Daniel Montford",
+    testimony:
+      "The staffs here are very friendly and accomodating. They will help you find the car base on your needs. (5/5)",
+  },
+];
+
+export default testimonials;
