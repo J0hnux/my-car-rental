4769a7feaeb13962ab16962c55f72e4b1b87b2aa
J0hnux <jc24.doliente@gmail.com>
2024-06-13 20:23:47 +0800

    Add image dynamically using mapped data from images.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,11 +1,16 @@
+import { useEffect, useRef, useState } from "react";
 import "./Hero.scss";
+import ImageSlider from "./ImageSlider";
+import images from "@/data/images";
 
 const Hero = () => {
   return (
     <div className="hero">
-      <div className="hero-image-slider">
-        <img src="/Hero.jpg" alt="luxury car image" />
-      </div>
+      <ImageSlider>
+        {images.map((image, index) => {
+          return <img key={index} src={image.imgURL} alt={image.imgAlt} />;
+        })}
+      </ImageSlider>
       <div className="hero-content">
         <h1>Looking to drive your own dream car?</h1>
         <button>Inquire Now</button>
