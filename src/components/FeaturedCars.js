ddd34c12b942aac44aadc7b9e6fe235f73e68774
J0hnux <jc24.doliente@gmail.com>
2024-06-17 21:47:22 +0800

    Scrapped image carousel plan and replaced with standard design

--- src/components/FeaturedCars.js
+++ src/components/FeaturedCars.js
@@ -1,43 +1,26 @@
+import FeaturedCar from "./FeaturedCar";
+import featuredCars from "../data/featuredcars";
 import "./FeaturedCars.scss";
 
 const FeaturedCars = () => {
   return (
-    <section className="featured container">
-      <h2>Featured Cars</h2>
-      <div className="image_carousel" aria-label="Featured Cars Image Carousel">
-        <div class="image-slide fade ">
-          <img
-            src="Featured_Cars/mercedes_benz_white.png"
-            alt="mercedes_benz"
+    <section className="featured bg-gray-200">
+      <div
+        className="featured-car-container"
+        aria-label="Featured Cars Image Carousel"
+      >
+        <h1>
+          <span>FEATURED</span> CARS FOR RENT
+        </h1>
+        {featuredCars.map((car, index) => (
+          <FeaturedCar
+            key={index}
+            imgURL={car.imgURL}
+            imgAlt={car.imgAlt}
+            name={car.name}
+            details={car.details}
           />
-          <div class="image-text">Mercedes Benz</div>
-        </div>
-
-        <div class="image-slide fade hidden">
-          <img src="Featured_Cars/ford_mustang_blue.png" alt="ford_mustang" />
-          <div class="image-text">Ford Mustang</div>
-        </div>
-
-        <div class="image-slide fade hidden">
-          <img
-            src="Featured_Cars/toyota_fortuner_white.png"
-            alt="toyota_fortuner"
-          />
-          <div class="image-text">Toyota Fortuner</div>
-        </div>
-
-        <a class="prev" aria-label="previous image" onclick="{plusSlides(-1)}">
-          &#10094;
-        </a>
-        <a class="next" aria-label="next image" onclick="{plusSlides(1)}">
-          &#10095;
-        </a>
-      </div>
-
-      <div className="dots">
-        <span class="dot" onclick="currentSlide(1)"></span>
-        <span class="dot" onclick="currentSlide(2)"></span>
-        <span class="dot" onclick="currentSlide(3)"></span>
+        ))}
       </div>
     </section>
   );
