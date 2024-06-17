b14862b9aebe18a138fd7f28eb8967b2fc78267b
J0hnux <jc24.doliente@gmail.com>
2024-06-17 21:45:42 +0800

    Added a separate data for featured cars

---
+++ src/data/featuredcars.js
@@ -0,0 +1,22 @@
+const featuredCars = [
+  {
+    imgURL: "/Featured_Cars/mercedes_benz_white.png",
+    imgAlt: "White Mercedes Benz",
+    name: "Mercedes Benz",
+    details: ["From ₱2,000 per hour", "3 passengers", "2022 model"],
+  },
+  {
+    imgURL: "/Featured_Cars/ford_mustang_blue.png",
+    imgAlt: "Blue Ford Mustang",
+    name: "Ford Mustang",
+    details: ["From ₱4,000 per hour", "1 passenger", "2021 model"],
+  },
+  {
+    imgURL: "/Featured_Cars/toyota_fortuner_white.png",
+    imgAlt: "White Toyota Fortuner",
+    name: "Toyota Fortuner",
+    details: ["From ₱5,000 per hour", "4 passengers", "2024 model"],
+  },
+];
+
+export default featuredCars;
