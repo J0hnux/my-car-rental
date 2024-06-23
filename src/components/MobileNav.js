2c9f23a6fe4d9a4d95db2531de6a8f66ab36f1d3
J0hnux <jc24.doliente@gmail.com>
2024-06-23 16:30:09 +0800

    Implemented mobile nav for low width screens

--- src/components/MobileNav.js
+++ src/components/MobileNav.js
@@ -1,9 +1,20 @@
+import Link from "next/link";
 import "./MobileNav.scss";
 
-const MobileNav = () => {
+const MobileNav = (props) => {
   return (
-    <nav className="mobile__nav">
-      <ul></ul>
+    <nav className={`mobile__nav ${props.isOpen ? "is-open" : ""} `}>
+      <ul>
+        <li>
+          <Link href="/About">ABOUT</Link>
+        </li>
+        <li>
+          <Link href="/Contact">CONTACT US</Link>
+        </li>
+        <li>
+          <Link href="/Login">INQUIRE NOW</Link>
+        </li>
+      </ul>
     </nav>
   );
 };
