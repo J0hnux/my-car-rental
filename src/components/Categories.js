592d942ef488bdc35188150b213466bb127343a8
J0hnux <jc24.doliente@gmail.com>
2024-06-23 15:06:19 +0800

    Categories section populated (planning to do a centered stack image carrousel)

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,5 +1,25 @@
+import "./Categories.scss";
+import CategoryCard from "./CategoryCard";
+import categories from "@/data/categories";
+
 const Categories = () => {
-  return <section className="categories container">Categories</section>;
+  return (
+    <section className="categories">
+      <h1>Categories</h1>
+      <div className="category-grid">
+        {categories.map((category, index) => {
+          return (
+            <CategoryCard
+              key={index}
+              imgURL={category.imgURL}
+              imgAlt={category.imgAlt}
+              category={category.category}
+            />
+          );
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default Categories;
