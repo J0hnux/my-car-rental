7a93b2f776f21717af1bae219cc90e166f772847
J0hnux <jc24.doliente@gmail.com>
2024-07-03 08:24:55 +0800

    Added Socials Panel and HomeHeader to these pages

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,15 +2,17 @@ import SocialsPanel from "@/components/SocialsPanel";
 import HomeHeader from "@/components/HomeHeader";
 import "@/app/globals.css";
 import "@/app/style.scss";
-import "./Contact.scss";
+import "./About.scss";
 
-const Contact = () => {
+const About = () => {
   return (
-    <div className="contact">
-      <SocialsPanel />
-      <HomeHeader />
-    </div>
+    <>
+      <div className="about-page">
+        <SocialsPanel />
+        <HomeHeader />
+      </div>
+    </>
   );
 };
 
-export default Contact;
+export default About;
