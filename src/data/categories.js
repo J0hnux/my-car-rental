6af6075d1f4cc39186633ba82f2dfbb2608423f2
J0hnux <jc24.doliente@gmail.com>
2024-07-03 08:25:15 +0800

    added categories data for testing

---
+++ src/data/categories.js
@@ -0,0 +1,29 @@
+const categories = [
+  {
+    imgURL: "/Hero.jpg",
+    imgAlt: "Offroad Car",
+    category: "OFF-ROADS",
+  },
+  {
+    imgURL: "/Hero.jpg",
+    imgAlt: "Sedan Car",
+    category: "SEDANS",
+  },
+  {
+    imgURL: "/Hero.jpg",
+    imgAlt: "Sports Car",
+    category: "SPORTS",
+  },
+  {
+    imgURL: "/Hero.jpg",
+    imgAlt: "SUV Car",
+    category: "SUVS",
+  },
+  {
+    imgURL: "/Hero.jpg",
+    imgAlt: "Pickup Car",
+    category: "PICKUPS",
+  },
+];
+
+export default categories;
