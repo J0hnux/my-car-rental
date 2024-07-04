5266ea96563b9117ec78d81018ea0abfa21bb502
J0hnux <jc24.doliente@gmail.com>
2024-07-05 02:39:08 +0800

    Basic data for category page

---
+++ src/data/category-page-details.js
@@ -0,0 +1,19 @@
+const categoryDetails = [
+  {
+    category: "offroads",
+    categoryImage: "",
+    categoryImageAlt: "",
+    categoryDescription: "Lorem ipsum dolor sit amet",
+    categoryFeaturedCars: [
+      {
+        featCar: "",
+        featCarImage: "",
+        featCarAlt: "",
+        featCarTitle: "",
+        featCarDetails: ["", "", ""],
+      },
+    ],
+  },
+];
+
+export default categoryDetails;
