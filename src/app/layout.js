6cfd7e2ecf0bfd2c5f58068ae80d2f9b323961dd
J0hnux <jc24.doliente@gmail.com>
2024-07-12 17:15:04 +0800

    Applied the new icon

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,6 +8,7 @@ import Providers from "@/components/Providers";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 // Prevent fontawesome from adding its CSS since we did it manually above:
 import { config } from "@fortawesome/fontawesome-svg-core";
+import Head from "next/head";
 config.autoAddCss = false; /* eslint-disable import/first */
 
 const inter = Inter({ subsets: ["latin"] });
@@ -20,6 +21,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <link rel="icon" href="/icon.ico" />
+      </Head>
       <body>
         <Providers>{children}</Providers>
       </body>
