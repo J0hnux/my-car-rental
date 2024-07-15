9b6b421a58d9fb6d029f4922632efc81d334fab2
J0hnux <jc24.doliente@gmail.com>
2024-07-15 20:11:35 +0800

    Captcha data for dynamic Captcha generation

---
+++ src/data/captcha.js
@@ -0,0 +1,34 @@
+const captchaData = [
+  {
+    imgURL: "/Captcha/captcha1.png",
+    imgAlt: "Captcha 1",
+    answer: "5",
+  },
+  {
+    imgURL: "/Captcha/captcha2.png",
+    imgAlt: "Captcha 2",
+    answer: "10",
+  },
+  {
+    imgURL: "/Captcha/captcha3.png",
+    imgAlt: "Captcha 3",
+    answer: "3",
+  },
+  {
+    imgURL: "/Captcha/captcha4.png",
+    imgAlt: "Captcha 4",
+    answer: "4",
+  },
+  {
+    imgURL: "/Captcha/captcha5.png",
+    imgAlt: "Captcha 5",
+    answer: "4",
+  },
+  {
+    imgURL: "/Captcha/captcha6.png",
+    imgAlt: "Captcha 6",
+    answer: "5",
+  },
+];
+
+export default captchaData;
