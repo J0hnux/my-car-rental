fde2ce0cc37775110642a843c265bc6250e4c0a3
J0hnux <jc24.doliente@gmail.com>
2024-07-15 15:22:50 +0800

    added position field

--- src/data/testimonials.js
+++ src/data/testimonials.js
@@ -3,6 +3,7 @@ const testimonials = [
     imgURL: "/Testimonials/my-pic2.jpeg",
     imgAlt: "clark's testimony",
     name: "Clark Doliente",
+    position: "CEO of Dolientech",
     testimony:
       "Rented a Mercedes Benz 3 months ago and it came right on time. I will definitely rent again if given the chance.",
   },
@@ -10,6 +11,7 @@ const testimonials = [
     imgURL: "/Testimonials/amara.png",
     imgAlt: "amara's testimony",
     name: "Amara Redfield",
+    position: "CEO of Amaran Pharmaceuticals",
     testimony:
       "The cars they put on rent here are top of the line and well maintained. I highly recommend this car rental shop.",
   },
@@ -17,6 +19,7 @@ const testimonials = [
     imgURL: "/Testimonials/daniel.png",
     imgAlt: "daniel's testimony",
     name: "Daniel Montford",
+    position: "Principal of Montford University",
     testimony:
       "The staffs here are very friendly and accomodating. They will help you find the car base on your needs. (5/5)",
   },
