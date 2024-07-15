78c92c5fb5b1e0f05e695262dc84fd8004b3c173
J0hnux <jc24.doliente@gmail.com>
2024-07-15 15:23:11 +0800

    Wrote creative reviews data

---
+++ src/data/about-reviews.js
@@ -0,0 +1,40 @@
+const aboutReviews = [
+  {
+    imgURL: "/AboutReviews/J.png",
+    imgAlt: "hello.png",
+    name: "John Marcus",
+    username: "marcus001128",
+    message:
+      "The last time I rented a car here was a Mercedes Benz, at first I thought it was going to be like other car rentals out there, but to my surprise it wasn't. Clark Cars is different from other car rentals out there, they value their customers more than their business, I asked the CEO for answers. The CEO told me \"We would have been long gone if it weren't for the customers.\" after hearing this, I realized the answer for my question. Anyway, that would not be my last time, for me this is just the beginning, I will definitely go back to rent more.",
+    numberOfLikes: "5",
+    numberOfReplies: "3",
+    numberOfShares: "8",
+    postDate: "7",
+  },
+  {
+    imgURL: "/Testimonials/my-pic2.jpeg",
+    imgAlt: "hello.png",
+    name: "John Clark",
+    username: "clark007",
+    message:
+      "We had an amazing day camping in the Palawan rainforest organized by Clark Cars. We enjoyed every bits of it, from the good quality car we have rented to the freebies that they gave us upon claiming the car we rented. We made a lot of good memories, and that one particular memory that we will not forget is choosing Clark Cars.",
+    numberOfLikes: "15",
+    numberOfReplies: "0",
+    numberOfShares: "3",
+    postDate: "8",
+  },
+  {
+    imgURL: "/Categories/Pickup.png",
+    imgAlt: "hello.png",
+    name: "Gregorio Del Pilar",
+    username: "gdp_carlover1859",
+    message:
+      "It was a very good experience to have rented a car with Car Rentals. They have everything that suits your needs. They even have cars tailored for People with Disabilities like myself. Speaking of PWDs, if you are one and with a valid PWD ID you can ask for a driver, once they have validated your PWD ID, they will provide you with a driver of their own suitable for your needs free of charge and you're off to go anywhere you want in the Philippines, all you have to do is ask the driver where you want to go.",
+    numberOfLikes: "101",
+    numberOfReplies: "57",
+    numberOfShares: "67",
+    postDate: "26",
+  },
+];
+
+export default aboutReviews;
