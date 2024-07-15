d278564dc8c54c4d4bf64d2126989d606950fa7d
J0hnux <jc24.doliente@gmail.com>
2024-07-15 15:21:49 +0800

    Added position

--- src/components/TestimonialCard.js
+++ src/components/TestimonialCard.js
@@ -6,7 +6,10 @@ const TestimonialCard = (props) => {
     <div className="testimonial-card">
       <Image src={props.imgUrl} alt={props.imgAlt} width={9000} height={9000} />
       <div className="testimonial-card-details">
-        <h3 className="mt-4">{props.name}</h3>
+        <div className="mt-2 testimonial-first">
+          <h3>{props.name}</h3>
+          <p>{props.position}</p>
+        </div>
         <p>{props.testimony}</p>
       </div>
     </div>
