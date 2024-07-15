dde0a5d284cba4a88d584a0387924cdecb318962
J0hnux <jc24.doliente@gmail.com>
2024-07-15 15:21:01 +0800

    Refined the markup and added icons

--- src/app/About/components/AboutReview/AboutReviewCard.js
+++ src/app/About/components/AboutReview/AboutReviewCard.js
@@ -4,13 +4,32 @@ const AboutReviewCard = (props) => {
   return (
     <div className="about-review-card">
       <div className="about-review-card-container">
-        <Image src={props.imgURL} height={25} width={25} alt={props.imgAlt} />
-        <p>{props.name}</p>
-        <p>{props.username}</p>
-        <p>{props.message}</p>
-        <span>Likes {props.numberOfLikes}</span>
-        <span>Replies {props.numberOfReplies}</span>
-        <span>Shares {props.numberOfShares}</span>
+        <div className="about-review-card-first">
+          <Image
+            className="arcf-image"
+            src={props.imgURL}
+            height={1000}
+            width={1000}
+            alt={props.imgAlt}
+          />
+          <div className="arcf-right">
+            <p className="arcf-name">{props.name}</p>
+            <p className="arcf-username">@{props.username}</p>
+          </div>
+        </div>
+        <p className="arc-user-message">{props.message}</p>
+        <div className="about-review-card-stats">
+          <span className="twt-likes-logo">❤️{props.numberOfLikes} Likes</span>
+          <span className="twt-ctrl-reply">
+            🛡️{props.numberOfReplies} Replies
+          </span>
+          <span className="twt-ctrl-share">
+            😎{props.numberOfShares} Shares
+          </span>
+        </div>
+        <p className="arc-postDate">
+          Posted {props.postDate} {props.postDate > 1 ? "days" : "day"} ago.
+        </p>
       </div>
     </div>
   );
