c4494b3f69c04e190abaebde2dbe0d43729131e7
J0hnux <jc24.doliente@gmail.com>
2024-07-27 22:31:44 +0800

    suddenly changed

--- src/app/page.js
+++ src/app/page.js
@@ -7,22 +7,19 @@ import Hero from "@/components/Hero";
 import HomeHeader from "@/components/HomeHeader";
 import SocialsPanel from "@/components/SocialsPanel";
 import Testimonials from "@/components/Testimonials";
-import { GlobalProvider } from "@/context/GlobalState";
 
 export default function Home() {
   return (
-    <GlobalProvider>
-      <div className="text-center w-full">
-        <SocialsPanel />
-        <HomeHeader />
-        <Hero />
-        <About />
-        <FeaturedCars />
-        <Categories />
-        {/* <Brands /> */}
-        <Testimonials />
-        <Footer />
-      </div>
-    </GlobalProvider>
+    <div className="text-center w-full">
+      <SocialsPanel />
+      <HomeHeader />
+      <Hero />
+      <About />
+      <FeaturedCars />
+      <Categories />
+      {/* <Brands /> */}
+      <Testimonials />
+      <Footer />
+    </div>
   );
 }
